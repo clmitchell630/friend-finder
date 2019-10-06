5144b14a6a079a8ba4adf685b28351256328f3bb
clmitchell630 <clmitchell630@gmail.com>
2019-10-05 23:28:04 -0500

    webpages more or less created

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var data = [
+    {
+        "name": "Patrick",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuRcYdvAf91LJVGqI_UANXErhA8qhUit4lSAZ451g0V5eP3Jrr",
+        "scores": [
+            "1",
+            "3",
+            "5",
+            "1",
+            "3",
+            "5",
+            "1",
+            "1",
+            "5",
+            "3"
+        ]
+    },
+
+];
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var path = require("path");
+var chosen = require("chosen-js");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
