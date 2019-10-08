d179ebe020d67966c467ea85e612cb485c8ac465
clmitchell630 <clmitchell630@gmail.com>
2019-10-07 22:13:00 -0500

    real questions, added friend match data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,83 @@
 module.exports = [
-
-
+    {
+        "name": "Patrick Star",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuRcYdvAf91LJVGqI_UANXErhA8qhUit4lSAZ451g0V5eP3Jrr",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "4",
+            "5",
+            "4",
+            "5",
+            "1",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Michael Scott",
+        "photo": "http://36.media.tumblr.com/3f336765fefe4f5b0f9246d733805d4d/tumblr_n43w4pCOm11sf2cljo1_500.jpg",
+        "scores": [
+            "1",
+            "1",
+            "5",
+            "5",
+            "3",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Peter Griffin",
+        "photo": "https://m.media-amazon.com/images/M/MV5BNGQyYWViOGQtNmI2ZC00NmYxLWFlZmEtY2M5ZTc0MWFhYTk0XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UY268_CR90,0,182,268_AL_.jpg",
+        "scores": [
+            "4",
+            "2",
+            "3",
+            "5",
+            "3",
+            "5",
+            "5",
+            "3",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Linda Belcher",
+        "photo": "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-Bobs-Burgers-John-Roberts.jpg",
+        "scores": [
+            "2",
+            "2",
+            "5",
+            "4",
+            "1",
+            "5",
+            "4",
+            "5",
+            "5",
+            "3"
+        ]
+    },
+    {
+        "name": "Leslie Knope",
+        "photo": "http://images6.fanpop.com/image/photos/38900000/Leslie-knope-pawnee-38912146-550-344.jpg",
+        "scores": [
+            "1",
+            "2",
+            "3",
+            "5",
+            "1",
+            "5",
+            "5",
+            "5",
+            "5",
+            "4"
+        ]
+    }
 ];
 
